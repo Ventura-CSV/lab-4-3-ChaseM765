eb9a604a5f8d95769154afad1ec0d7f847b3997f
ChaseM765 <ChaseM765>
2024-06-28 00:10:25 -0700

    while loop added

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 def main():
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
-    total = 0
 
+    total = 0
+    x = 0 
+    while x<5:
+        num = int(input('Enter value: '))
+        total += num
+        x += 1
+    print(total)
+    
     ########################################
     # Do not delete the return statement
     ########################################
